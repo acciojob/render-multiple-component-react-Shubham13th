863102956740548c3617ddadb7341e957d7db670
shubham13th <shubhamsharmajcc@gmail.com>
2025-09-02 15:04:47 +0530

    first comit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Gym Management System</h1>
+      <h6 data-ns-test="project-description">A gym management system is a software application designed to automate and streamline the operations of a fitness center, replacing manual processes with a digital solution for managing members, classes, trainers, payments, and equipment. Key objectives include enhancing efficiency, improving customer service through features like online registration and class bookings, enabling easy data management and retrieval, providing detailed reporting for administrators, and offering a secure platform with user authentication for authorized staff and members.
+      </h6>
+    </div>
   )
 }
 
